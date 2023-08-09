96f5af1227a5ac2c0f5fee2ac6a935d60bcae9d3
zlongx <1251885400@qq.com>
2023-08-09 11:31:45 +0800

    更新

---
+++ generics/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+// 泛型类型
+type List[T any] struct {
+	next  *List[T]
+	value T
+}
+
+// 泛型函数
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	l := List[int]{}
+	_ = l
+
+	m1 := make(map[string]int64)
+	m2 := make(map[string]float64)
+
+	m1["A"] = 1
+	m1["B"] = 2
+	m1["C"] = 3
+	fmt.Println(SumIntsOrFloats(m1)) // 6
+
+	m2["A"] = 1.2
+	m2["B"] = 2.3
+	m2["C"] = 3.4
+	fmt.Println(SumIntsOrFloats(m2)) // 6.9 或 6.8999999999999995 <- 奇怪的结果
+
+}
